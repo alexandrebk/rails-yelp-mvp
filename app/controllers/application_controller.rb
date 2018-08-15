1f8c66e33529b01eaab0860c1f5ad9f19a5023d0
alexandrebk <alexandre.b2506@gmail.com>
2018-08-15 09:33:57 +0200

    bootsrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  CLASSES = %w(restaurant review)
+
+  # CLASSES.each do |toto|
+
+
+  #   puts "debug"
+
+  #   p toto
+
+  #   def index
+  #     @restaurants = toto.capitalize.constantize.all
+  #   end
+  #   define_method("set_#{toto}") do
+  #     @restaurant = toto.capitalize.constantize.find(params[:id])
+  #     # fred.instance_variable_set(:@a, 'dog')
+  #   end
+
+  # end
+
 end
